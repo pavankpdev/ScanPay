28b4ad3443bc0feb23ebbb9653432cd3ee75050b
pavanleton <pavankumarkpkp@gmail.com>
2023-08-18 18:40:17 +0530

    feat(router): Setup Navigation

--- App.tsx
+++ App.tsx
@@ -2,16 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Auth from "./screens/Auth";
 import MVerify from "./screens/mnemonic/Verify";
-
+import MDisplay from "./screens/mnemonic/Display";
+import Wallet from "./screens/Wallet";
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 const queryClient = new QueryClient();
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
       <QueryClientProvider client={queryClient}>
           <PaperProvider>
-            <MVerify />
+              <NavigationContainer>
+                  <Stack.Navigator
+                      initialRouteName="Auth"
+                      screenOptions={{
+                          headerShown: false,
+                      }}
+                  >
+                      <Stack.Screen name="Auth" component={Auth} />
+                      <Stack.Screen name="DisplayMnemonic" component={MDisplay} />
+                      <Stack.Screen name="VerifyMnemonic" component={MVerify} />
+                      <Stack.Screen name="Wallet" component={Wallet} />
+                  </Stack.Navigator>
+              </NavigationContainer>
           </PaperProvider>
       </QueryClientProvider>
   );
